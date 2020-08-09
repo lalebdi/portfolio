f76093ea3963aeecb8a4fc51ee517c547580b1c5
lalebdi <loai.alebdi@gmail.com>
2020-08-08 22:07:37 -0400

    The router is set :)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import './App.css';
-import NavBar from './Components/NavBar'
+import NavBar from './Components/NavBar';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import NotFound from './Components/NotFound';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <NavBar />
       <h1> Cocked, Locked and ready to Rock</h1>
+      <div>
+      <Switch>
+      <Route exact path="/" component={ Home } >
+
+      </Route>
+      <Route path="/about" component={ About } >
+
+      </Route>
+      <Route path="/contact" component={ Contact } >
+
+      </Route>
+      <Route component={ NotFound }>
+
+      </Route>
+      </Switch>
+      </div>
+      </Router>
     </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-export default class Contact extends Component {
+export default class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Contact component</h1>
+                <h1>Home component</h1>
             </div>
         )
     }
