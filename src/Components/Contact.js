4db0b5aae627fdd07325bd1e970d90b1e6a2b44f
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-21 20:14:30 -0500

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,7 +4,7 @@ export default class Contact extends Component {
     render() {
         return (
             <div>
-                <h1>Contact component</h1>
+                <h1></h1>
             </div>
         )
     }
