720e8ee347f9d020c9d8b545ee8053a48fb8a427
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-21 20:13:28 -0500

    Update NotFound.js

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NotFound() {
     return (
         <div>
-            <h1>Not Found component</h1>
+            <h1></h1>
         </div>
     )
 }
