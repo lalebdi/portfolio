e77b6fd9ffd6dc5a5cc84046b04b9eb29d28af96
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-29 20:50:25 -0500

    Update serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -30,7 +30,7 @@ export function register(config) {
           );
         });
       } else {
-        // Is not localhost. Just register service worker
+
         registerValidSW(swUrl, config);
       }
     });
