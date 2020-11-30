242c281f35976931372d6ce3cedcdde6ff5a173f
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-29 20:49:55 -0500

    Update serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -24,7 +24,7 @@ export function register(config) {
 
         checkValidServiceWorker(swUrl, config);
 
-        // Add some additional logging to localhost, pointing developers to the
+
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
           console.log(
