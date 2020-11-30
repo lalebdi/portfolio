b74567b909e631bfaf311e1b84dd15854208bffa
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-29 20:49:32 -0500

    Update serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -14,8 +14,6 @@ export function register(config) {
 
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
-
-
       return;
     }
 
